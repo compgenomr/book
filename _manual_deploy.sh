6beb98060b6d7a09ddf07942f9c2d7213210b860
Altuna Akalin <aakalin@gmail.com>
2019-05-09 15:58:56 +0200

    manual deploy script, travis TOs

---
+++ _manual_deploy.sh
@@ -0,0 +1,8 @@
+# works on master branch
+# use it after bookdown::render() or knitting index.Rmd
+git clone -b gh-pages   https://github.com/compgenomr/book.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add *
+git commit -m "Update the book manually"
+git push origin gh-pages
