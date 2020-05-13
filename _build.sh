9f4d8d124ebefa4767866d3b158d0bb934c4f861
Altuna Akalin <aakalin@gmail.com>
2020-05-13 15:41:31 +0200

    fixing the checklist issues, chapt 6

--- _build.sh
+++ _build.sh
@@ -4,3 +4,4 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',output_dir='book_pdf')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session=TRUE)"
