761dd1791bfb4503ef949f63ca26660ba8691670
feizhao <zhaofei920810@gmail.com>
2021-07-16 10:55:02 +0800

    update4

--- _build.sh
+++ _build.sh
@@ -2,8 +2,14 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',output_dir='book_pdf')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session=TRUE)"
+# render gitbook
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session=TRUE)"
 
-Rscript -e "bookdown::preview_chapter('01-intro2Genomics.Rmd', 'bookdown::pdf_book',new_session=TRUE)"
+# render pdf
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',output_dir='book_pdf')"
+
+# compile each chapter separetely and merge
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session=TRUE)"
+
+# compile one chapter
+#Rscript -e "bookdown::preview_chapter('01-intro2Genomics.Rmd', 'bookdown::pdf_book',new_session=TRUE)"
